9f7dd05d44248a17841532b78931f8f1caa6bf29
Plantin <vincent.agullo1@gmail.com>
2020-06-11 23:33:55 +0200

    Create basic project

---
+++ cmake-build-debug/CMakeFiles/JV_Engine.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JV_Engine.dir/main.cpp.o"
+  "JV_Engine"
+  "JV_Engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/JV_Engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
